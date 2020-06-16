aa1a5b6c21b3423153b3ac90702a3503dd89643d
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2020-06-16 13:17:26 +0200

    Basic routing

--- src/MapContainer/MapContainer.tsx
+++ src/MapContainer/MapContainer.tsx
@@ -1,34 +1,82 @@
 import './MapContainer.css';
-import React, { useState } from 'react';
-import { Map, TileLayer } from 'react-leaflet';
+import React, { useState, useEffect } from 'react';
+import { Map, TileLayer, Marker, Polyline } from 'react-leaflet';
 import { LocationEvent } from 'leaflet';
-import { getGeolocation } from '../util/openStreetmaps';
+import { getGeolocation, getPath } from '../util/openStreetmaps';
+
+const getRandomTriangleAt = (at: number) => {
+  const angle = Math.random() * Math.PI * 2;
+  const length = 0.5;
+  const kmPoints: Array<[number, number]> = [
+    [0, 0],
+    [length * 1.22, -length / 2],
+    [length * 1.22, length / 2],
+  ].map((v) => {
+    return [v[0] * Math.cos(angle) - v[1] * Math.sin(angle), v[0] * Math.sin(angle) + v[1] * Math.cos(angle)];
+  });
+
+  return kmPoints.map(convertKmToAngles, at);
+};
+
+const convertKmToAngles = (vec: [number, number], at: number): [number, number] => {
+  const lat = vec[0] / 110.547;
+  const lon = vec[1] / (111.32 * Math.cos(((at + lat) * Math.PI) / 180));
+  return [lat, lon];
+};
 
 function MapContainer() {
   const [state, setState] = useState({
     lat: 0,
     lng: 0,
     zoom: 3,
+    markers: new Array<[number, number]>(),
+    path: new Array<[number, number]>(),
   });
 
   const position = { lat: state.lat, lng: state.lng };
 
-  getGeolocation()
-    .then((position) => {
-      setState({
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-        zoom: 16,
-      });
-    })
-    .catch(() => null); // Silently ignore
+  useEffect(() => {
+    getGeolocation()
+      .then((position) => {
+        const coords = getRandomTriangleAt(position.coords.latitude).map(
+          (v) => [v[0] + position.coords.latitude, v[1] + position.coords.longitude] as [number, number],
+        );
+
+        setState((state) => ({
+          ...state,
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+          zoom: 16,
+          markers: coords,
+        }));
+        const promises = coords.map((v, i) =>
+          getPath(coords[i][0], coords[i][1], coords[(i + 1) % coords.length][0], coords[(i + 1) % coords.length][1]),
+        );
+        return Promise.all(promises);
+      })
+      .then((paths) => {
+        const path: [number, number][] = paths
+          .reduce<Array<[number, number]>>((p, current) => p.concat(current.coordinates), [])
+          .map((v) => [v[1], v[0]]);
+
+        console.log(paths.reduce<number>((p, curr) => p + parseFloat(curr.properties.distance), 0));
+        console.log(path);
+
+        setState((state) => ({
+          ...state,
+          path,
+        }));
+      })
+      .catch(() => null); // Silently ignore
+  }, []);
 
   const onlocationfound = (ev: LocationEvent) => {
-    setState({
+    setState((state) => ({
+      ...state,
       lat: ev.latlng.lat,
       lng: ev.latlng.lng,
       zoom: 16,
-    });
+    }));
   };
 
   return (
@@ -37,6 +85,16 @@ function MapContainer() {
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {state.markers.map((m, i) => (
+        <Marker key={i} position={m} />
+      ))}
+      {/* {state.markers.map((m, i) => (
+        (i === 0)
+        ? null
+        : 
+      ))} */}
+      {/* {state.markers.length > 0 ? <Polyline positions={[...state.markers, state.markers[0]]} /> : null} */}
+      <Polyline positions={state.path} />
     </Map>
   );
 }
--- src/util/openStreetmaps.ts
+++ src/util/openStreetmaps.ts
@@ -21,15 +21,25 @@ export const getStartingNodes = (lat: number, lng: number): Promise<unknown> => 
   );
 };
 
-export const getPath = (
-  flat: number,
-  flng: number,
-  tlat: number,
-  tlng: number,
-): Promise<{ coordinates: Array<[number, number]> }> => {
-  return Axios.get(
-    `http://www.yournavigation.org/api/1.0/gosmore.php?format=geojson&flat=${flat}&flon=${flng}&tlat=${tlat}&tlon=${tlng}&v=foot&fast=0&layer=mapnik`,
-  );
+type RoutingData = { coordinates: Array<[number, number]>; properties: { distance: string } };
+
+export const getPath = (flat: number, flng: number, tlat: number, tlng: number): Promise<RoutingData> => {
+  return Axios.get<RoutingData>(
+    `https://wandering-application.herokuapp.com/api/1.0/gosmore.php?format=geojson&flat=${flat}&flon=${flng}&tlat=${tlat}&tlon=${tlng}&fast=0&layer=mapnik`, // &v=foot
+  ).then((res) => res.data);
 };
+
+export function measure(lat1: number, lon1: number, lat2: number, lon2: number) {
+  // generally used geo measurement function
+  const R = 6378.137; // Radius of earth in KM
+  const dLat = (lat2 * Math.PI) / 180 - (lat1 * Math.PI) / 180;
+  const dLon = (lon2 * Math.PI) / 180 - (lon1 * Math.PI) / 180;
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos((lat1 * Math.PI) / 180) * Math.cos((lat2 * Math.PI) / 180) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c;
+  return d * 1000; // meters
+}
 
 // TODO: Implement finding routes
