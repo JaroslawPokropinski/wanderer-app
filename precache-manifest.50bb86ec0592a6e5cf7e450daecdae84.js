efdb779ba00994b3b5326c1cb814554ebdafee1d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-19 11:10:10 +0000

    Deploy JaroslawPokropinski/wanderer-app to github.com/JaroslawPokropinski/wanderer-app.git:gh-pages

---
+++ precache-manifest.50bb86ec0592a6e5cf7e450daecdae84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a85b902dc1550e2bab1fc46805893f5d",
+    "url": "/wanderer-app/index.html"
+  },
+  {
+    "revision": "e20e3db3982f5473aac8",
+    "url": "/wanderer-app/static/css/main.2c05c248.chunk.css"
+  },
+  {
+    "revision": "519539c9739aa4feb9ab",
+    "url": "/wanderer-app/static/js/2.d16c2e34.chunk.js"
+  },
+  {
+    "revision": "278be41389e3536a5347a9018737729d",
+    "url": "/wanderer-app/static/js/2.d16c2e34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e20e3db3982f5473aac8",
+    "url": "/wanderer-app/static/js/main.05a4ad69.chunk.js"
+  },
+  {
+    "revision": "cad5dfe788cee925de3f",
+    "url": "/wanderer-app/static/js/runtime-main.36254df8.js"
+  }
+]);
